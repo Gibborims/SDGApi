7d2f4453066b275560a2ec04118b443b078ff16a
Francis Nartey <narteyf7@gmail.com>
2020-04-12 09:42:12 +0000

    heroku second push

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
+
+
+
 
 
 @app.route("/")
 def home():
     return "Hello, Flask!"
+
+
+@app.route('/api/v1/on-covid-19/<format_type>', methods = ['POST'])
+def the_estimator_api(format_type):
+    if request.method == 'POST':
+        data = request.data
+            if format_type == 'json':
+                return data
+            elif format_type == 'xml':
+                return data
+            else:
+                return data
+    else:
+        return "Sorry, the request method is not a POST request."
+
+
+
+
+
+
+
+
+
+
+
 
 
 
