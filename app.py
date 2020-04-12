c64ef57251ebc23620429629590008b5308be0e9
Francis Nartey <narteyf7@gmail.com>
2020-04-12 13:43:01 +0000

    heroku push 12

--- app.py
+++ app.py
@@ -6,6 +6,30 @@ import math
 import json
 import ast
 
+def json2xml(json_obj, line_padding=""):
+    result_list = list()
+
+    json_obj_type = type(json_obj)
+
+    if json_obj_type is list:
+        for sub_elem in json_obj:
+            result_list.append(json2xml(sub_elem, line_padding))
+
+        return "\n".join(result_list)
+
+    if json_obj_type is dict:
+        for tag_name in json_obj:
+            sub_obj = json_obj[tag_name]
+            result_list.append("%s<%s>" % (line_padding, tag_name))
+            result_list.append(json2xml(sub_obj, "\t" + line_padding))
+            result_list.append("%s</%s>" % (line_padding, tag_name))
+
+        return "\n".join(result_list)
+
+    return "%s%s" % (line_padding, json_obj)
+
+
+
 def estimator(data):
   output_data = {}
   impact = {}
@@ -161,7 +185,7 @@ def the_estimator_api(format_type):
             return estimator(data)
         elif format_type == 'xml':
             data = ast.literal_eval(request.data.decode("utf-8"))
-            return Response(estimator(data), mimetype='text/xml')
+            return json2xml(Response(estimator(data), mimetype='text/xml')) #Response(estimator(data), mimetype='text/xml')
         else:
             return "This format parameter is not allowed."
     else:
